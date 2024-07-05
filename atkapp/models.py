3749ee3829557f6d87d604656964df81e62ba396
brobaray <brobaray@gmail.com>
2024-07-05 14:57:46 +0700

    stok_brg -- kode flag detail

--- atkapp/models.py
+++ atkapp/models.py
@@ -51,6 +51,9 @@ class Pengeluaran(models.Model):
     personal_id=models.ForeignKey(Personal, on_delete=models.PROTECT,null=True)
     qty=models.IntegerField()
 
+# KODE
+# 1 = pembelian, 2 = pengeluaran, 3 = koreksi pembelian, 4 = koreksi penjualan
+
 class Stok_brg(models.Model):
     tgl_transaksi=models.DateTimeField()
     master_barang_id=models.ForeignKey(Master_barang,on_delete=models.PROTECT)
@@ -58,8 +61,8 @@ class Stok_brg(models.Model):
     qty_keluar=models.IntegerField()
     stok=models.IntegerField()
     kode=models.IntegerField()
-    person=models.CharField(max_length=255)
-    created_at=models.DateTimeField(auto_now_add=True)
+    person=models.CharField(max_length=255, null= True)
+    created_at=models.DateTimeField(auto_now_add=True, null=True)
 
 class Temporary_pembelian(models.Model):
     tgl_beli=models.DateTimeField()
